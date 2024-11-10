c6303e540d35adea10f441832f810d3484fc6938
ifaxec <xecfahim.ahmed@gmail.com>
2024-11-10 16:20:09 +0000

    updated app.js

--- app.js
+++ app.js
@@ -1,21 +1,43 @@
 let mykey = config.MY_KEY;
 
+
 const photoList = document.getElementById('photo-list')
-console.log("🚀 ~ photoList:", photoList);
-
-
 
 const fetchPhotos = async () => {
-    const token = 'your-auth-token';
 
-    const response = await fetch('https://api.pexels.com/v1/search?query=nature&per_page=1', {
-        method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${mykey}`,
-            'Content-Type': 'application/json'
+    const photoDiv = document.createElement('div')
+    try {
+
+        const response = await fetch('https://api.pexels.com/v1/search?query=nature&per_page=1', {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${mykey}`,
+                'Content-Type': 'application/json'
+            }
+
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error:${response.status}`)
         }
-    });
 
-    const data = await response.json();
-    return data;
+        const data = await response.json();
+        console.log("🚀 ~ data:", data);
+        return data;
+
+
+
+
+    } catch (error) {
+        console.error('Error fetching photos:', error)
+    }
+
+
+
+
 }
+
+fetchPhotos();
+
+
+
+
